9ca8f5ea172e74d19dc02195d3d72e96df1231c9
nigelrmtaylor <31902847+nigelrmtaylor@users.noreply.github.com>
2026-07-13 17:52:53 +0100

    updated links

--- app-files/data.js
+++ app-files/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-side_1536",
-      "name": "side_1536",
+      "id": "0-bureau",
+      "name": "bureau",
       "levels": [
         {
           "tileSize": 256,
@@ -24,16 +24,29 @@ var APP_DATA = {
       ],
       "faceSize": 1536,
       "initialViewParameters": {
+        "yaw": 2.6080877347608995,
         "pitch": 0,
-        "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -0.5861661723599276,
+          "pitch": 0.4956080777206573,
+          "rotation": 0,
+          "target": "9-sejour"
+        },
+        {
+          "yaw": -1.5163079907447603,
+          "pitch": 0.21802760685055134,
+          "rotation": 12.566370614359176,
+          "target": "3-chambre3"
+        }
+      ],
       "infoHotspots": []
     },
     {
-      "id": "1-entree",
-      "name": "entree",
+      "id": "1-chambre1",
+      "name": "chambre1",
       "levels": [
         {
           "tileSize": 256,
@@ -61,29 +74,23 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.34608238159204063,
-          "pitch": 0.06401783491592994,
+          "yaw": 2.887622791435682,
+          "pitch": 0.15382759646324118,
           "rotation": 0,
-          "target": "2-sejour"
+          "target": "6-dgt"
         },
         {
-          "yaw": 0.34608246879321847,
-          "pitch": 0.06401780273581892,
+          "yaw": 2.8827011263729077,
+          "pitch": 0.2817842489283713,
           "rotation": 0,
-          "target": "2-sejour"
-        },
-        {
-          "yaw": 0.3247953083421642,
-          "pitch": -0.38641120709686305,
-          "rotation": 0,
-          "target": "2-sejour"
+          "target": "8-saldeau"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-sejour",
-      "name": "sejour",
+      "id": "2-chambre2",
+      "name": "chambre2",
       "levels": [
         {
           "tileSize": 256,
@@ -144,8 +151,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "4-chambre1",
-      "name": "chambre1",
+      "id": "4-chambre4",
+      "name": "chambre4",
       "levels": [
         {
           "tileSize": 256,
@@ -175,8 +182,8 @@ var APP_DATA = {
       "infoHotspots": []
     },
     {
-      "id": "5-chambre2",
-      "name": "chambre2",
+      "id": "5-cuisine",
+      "name": "cuisine",
       "levels": [
         {
           "tileSize": 256,
@@ -202,12 +209,19 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -0.6167284678277696,
+          "pitch": 0.042540824877136174,
+          "rotation": 0,
+          "target": "9-sejour"
+        }
+      ],
       "infoHotspots": []
     },
     {
-      "id": "6-bureau",
-      "name": "bureau",
+      "id": "6-dgt",
+      "name": "dgt",
       "levels": [
         {
           "tileSize": 256,
@@ -233,11 +247,216 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 0.663683311241364,
+          "pitch": 0.5271542466004053,
+          "rotation": 0,
+          "target": "7-entree"
+        },
+        {
+          "yaw": -2.806139069519828,
+          "pitch": 0.18627286910037633,
+          "rotation": 0,
+          "target": "4-chambre4"
+        },
+        {
+          "yaw": -1.6378218019133737,
+          "pitch": 0.3507898734873045,
+          "rotation": 0,
+          "target": "2-chambre2"
+        },
+        {
+          "yaw": 2.6084119934878487,
+          "pitch": 0.16276207979305113,
+          "rotation": 0,
+          "target": "1-chambre1"
+        },
+        {
+          "yaw": 0.21249128785235882,
+          "pitch": 0.028083993435206622,
+          "rotation": 0,
+          "target": "9-sejour"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-entree",
+      "name": "entree",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5977582954107596,
+          "pitch": 0.023977976013991764,
+          "rotation": 0,
+          "target": "5-cuisine"
+        },
+        {
+          "yaw": 0.4425402475557476,
+          "pitch": -0.3837486623206807,
+          "rotation": 0,
+          "target": "0-bureau"
+        },
+        {
+          "yaw": 0.45685975251711497,
+          "pitch": 0.22437424707057474,
+          "rotation": 0,
+          "target": "9-sejour"
+        },
+        {
+          "yaw": -1.6171060859032576,
+          "pitch": 0.09001170978647366,
+          "rotation": 0,
+          "target": "2-chambre2"
+        },
+        {
+          "yaw": -2.1246466880131685,
+          "pitch": 0.13329514526181185,
+          "rotation": 0,
+          "target": "1-chambre1"
+        },
+        {
+          "yaw": -1.9159180533383076,
+          "pitch": 0.14146803638873884,
+          "rotation": 0,
+          "target": "4-chambre4"
+        },
+        {
+          "yaw": -1.933676273529974,
+          "pitch": 0.4186219188466236,
+          "rotation": 0,
+          "target": "6-dgt"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-saldeau",
+      "name": "saldeau",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3519448645447785,
+          "pitch": 0.07527573198124315,
+          "rotation": 0,
+          "target": "1-chambre1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-sejour",
+      "name": "sejour",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1536,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.27528280788591,
+          "pitch": -0.7091730453439773,
+          "rotation": 0,
+          "target": "0-bureau"
+        },
+        {
+          "yaw": 0.6610151605170991,
+          "pitch": 0.3205314447364387,
+          "rotation": 0,
+          "target": "5-cuisine"
+        },
+        {
+          "yaw": -2.888521251827706,
+          "pitch": 0.4302878860121506,
+          "rotation": 0,
+          "target": "7-entree"
+        },
+        {
+          "yaw": -2.657527367079826,
+          "pitch": 0.32510054468676586,
+          "rotation": 0,
+          "target": "6-dgt"
+        },
+        {
+          "yaw": -2.676667716027616,
+          "pitch": 0.1094613915077165,
+          "rotation": 0,
+          "target": "1-chambre1"
+        }
+      ],
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "juan",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
